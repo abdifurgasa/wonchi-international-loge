332f56728e9f4e7043de32d3aa4c1276876e4a62
abdifurgasa <abdifurgasa2nd@gmail.com>
2026-02-26 20:59:48 +0300

    Update room.js

--- js/room.js
+++ js/room.js
@@ -1,1 +1,41 @@
+let rooms = JSON.parse(localStorage.getItem("rooms")) || [];
 
+function addRoom(){
+    let name = prompt("Enter Room Name");
+    let price = prompt("Enter Room Price");
+
+    if(name && price){
+        rooms.push({
+            name:name,
+            price:price
+        });
+
+        localStorage.setItem("rooms",JSON.stringify(rooms));
+        alert("Room Added Successfully");
+        showRooms();
+    }
+}
+
+function showRooms(){
+    let container = document.getElementById("roomList");
+
+    if(!container) return;
+
+    container.innerHTML = "";
+
+    rooms.forEach((r,i)=>{
+        container.innerHTML += `
+        <div class="card">
+            <h3>${r.name}</h3>
+            <p>Price: $${r.price}</p>
+            <button onclick="deleteRoom(${i})">Delete</button>
+        </div>
+        `;
+    });
+}
+
+function deleteRoom(index){
+    rooms.splice(index,1);
+    localStorage.setItem("rooms",JSON.stringify(rooms));
+    showRooms();
+}

51b4b6fe37d129ca74d29681c34beb314f4e141e
abdifurgasa <abdifurgasa2nd@gmail.com>
2026-02-26 21:29:47 +0300

    Update finance.js

--- js/finance.js
+++ js/finance.js
@@ -1,1 +1,14 @@
+function updateFinance(){
 
+    let totalIncome = bookings.reduce((sum,b)=>sum + b.price,0);
+
+    let financeBox = document.getElementById("financeData");
+
+    if(financeBox){
+        financeBox.innerHTML = `
+        <h3>💰 Finance Report</h3>
+        <p>Total Income: $${totalIncome}</p>
+        <p>Total Bookings: ${bookings.length}</p>
+        `;
+    }
+}

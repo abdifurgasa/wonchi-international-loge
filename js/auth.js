d8c36c4102a70fcb8800e31dc46f0448af189b5e
abdifurgasa <abdifurgasa2nd@gmail.com>
2026-02-26 20:57:46 +0300

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -3,11 +3,19 @@ function login(){
     const pass = document.getElementById('password').value;
     const msg = document.getElementById('msg');
 
-    if(user === 'admin' && pass === 'admin123'){
-        msg.innerText = "Login Successful!";
+    if(user === "admin" && pass === "admin123"){
+        localStorage.setItem("loggedIn","true");
         window.location.href = "dashboard.html";
-    } else {
-        msg.innerText = "Invalid Username or Password!";
+    }else{
+        msg.innerText = "Invalid Username or Password";
         msg.style.color = "red";
+    }
+}
+
+window.onload = function(){
+    if(window.location.pathname.includes("dashboard")){
+        if(localStorage.getItem("loggedIn") !== "true"){
+            window.location.href = "login.html";
+        }
     }
 }

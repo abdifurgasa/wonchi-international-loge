06369716a255360de076edd32386a7c72d732a55
abdifurgasa <abdifurgasa2nd@gmail.com>
2026-02-27 17:20:35 +0300

    Update models.js

--- js/core/models.js
+++ js/core/models.js
@@ -1,1 +1,18 @@
+class Room {
+    constructor(number, type, price) {
+        this.id = Date.now();
+        this.number = number;
+        this.type = type;
+        this.price = price;
+        this.status = "available";
+    }
+}
 
+class Customer {
+    constructor(name, phone) {
+        this.id = Date.now();
+        this.name = name;
+        this.phone = phone;
+        this.date = new Date();
+    }
+}

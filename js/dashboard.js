43e2f474cde2333fa4c59b964ce9f9f9fe84ae5e
abdifurgasa <abdifurgasa2nd@gmail.com>
2026-02-27 17:32:56 +0300

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,3 +1,21 @@
+window.onload = function () {
+    Database.init();
+    loadDashboard();
+};
+
+function loadDashboard() {
+    const db = Database.getData();
+
+    document.querySelectorAll(".card p")[0].innerText = db.rooms.length;
+    document.querySelectorAll(".card p")[3].innerText = db.customers.length;
+
+    const totalRevenue = db.finance.reduce((sum, item) => sum + item.amount, 0);
+    document.querySelectorAll(".card p")[2].innerText = totalRevenue + " ETB";
+
+    const availableRooms = db.rooms.filter(r => r.status === "available").length;
+    document.querySelectorAll(".card p")[1].innerText = availableRooms;
+}
+
 function navigate(page) {
     window.location.href = "pages/" + page + ".html";
 }

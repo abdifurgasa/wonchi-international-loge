3090c44356ffcdce8a8fba3b7f067d72450e89cd
abdifurgasa <abdifurgasa2nd@gmail.com>
2026-02-27 17:20:02 +0300

    Update database.js

--- js/core/database.js
+++ js/core/database.js
@@ -1,1 +1,21 @@
+class Database {
+    static init() {
+        if (!localStorage.getItem(CONFIG.storageKey)) {
+            const initialData = {
+                rooms: [],
+                customers: [],
+                bookings: [],
+                finance: []
+            };
+            localStorage.setItem(CONFIG.storageKey, JSON.stringify(initialData));
+        }
+    }
 
+    static getData() {
+        return JSON.parse(localStorage.getItem(CONFIG.storageKey));
+    }
+
+    static saveData(data) {
+        localStorage.setItem(CONFIG.storageKey, JSON.stringify(data));
+    }
+}

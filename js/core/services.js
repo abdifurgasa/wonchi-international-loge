01fe3c45bf08a7789dc68357b7cde20a7be00a37
abdifurgasa <abdifurgasa2nd@gmail.com>
2026-02-27 17:21:34 +0300

    Update services.js

--- js/core/services.js
+++ js/core/services.js
@@ -1,1 +1,25 @@
+class RoomService {
 
+    static addRoom(room) {
+        const db = Database.getData();
+        db.rooms.push(room);
+        Database.saveData(db);
+    }
+
+    static getRooms() {
+        return Database.getData().rooms;
+    }
+}
+
+class CustomerService {
+
+    static addCustomer(customer) {
+        const db = Database.getData();
+        db.customers.push(customer);
+        Database.saveData(db);
+    }
+
+    static getCustomers() {
+        return Database.getData().customers;
+    }
+}

0ec09b30cc09a74ae11e7a8c630d47c1b7450cfc
abdifurgasa <abdifurgasa2nd@gmail.com>
2026-03-01 11:25:15 +0300

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,16 +1,18 @@
 import { auth } from "./firebase.js";
-import { signInWithEmailAndPassword } 
+import { signInWithEmailAndPassword }
 from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
-window.login = function() {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
+window.login = function(){
 
-  signInWithEmailAndPassword(auth, email, password)
-    .then(() => {
-      window.location.href = "dashboard.html";
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    signInWithEmailAndPassword(auth,email,password)
+    .then(()=>{
+        window.location.href="dashboard.html";
     })
-    .catch((error) => {
-      document.getElementById("error").innerText = error.message;
+    .catch(err=>{
+        document.getElementById("error").innerText=err.message;
     });
+
 }

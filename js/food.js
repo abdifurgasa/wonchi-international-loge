3fd365233a39e35ebf6d280491d7b43cf0e3fc6f
abdifurgasa <abdifurgasa2nd@gmail.com>
2026-03-03 12:38:26 +0300

    Update food.js

--- js/food.js
+++ js/food.js
@@ -1,1 +1,36 @@
+import { db } from "./firebase.js";
 
+import {
+collection,
+addDoc,
+getDocs
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+/* ================= FOOD SYSTEM ================= */
+
+export async function loadFoods(){
+
+const snapshot =
+await getDocs(collection(db,"foods"));
+
+const foodList =
+document.getElementById("foodList");
+
+if(!foodList) return;
+
+foodList.innerHTML="";
+
+snapshot.forEach(docSnap=>{
+const data = docSnap.data();
+
+foodList.innerHTML+=`
+<div class="card">
+
+<h3>${data.name}</h3>
+<p>Price: $${data.price}</p>
+
+</div>
+`;
+});
+
+}

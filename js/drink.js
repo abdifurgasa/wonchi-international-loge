9e5402613cf47a8ad0d8490938daca666c7cbc03
abdifurgasa <abdifurgasa2nd@gmail.com>
2026-03-03 12:38:56 +0300

    Update drink.js

--- js/drink.js
+++ js/drink.js
@@ -1,1 +1,35 @@
+import { db } from "./firebase.js";
 
+import {
+collection,
+getDocs
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+/* ================= DRINK SYSTEM ================= */
+
+export async function loadDrinks(){
+
+const snapshot =
+await getDocs(collection(db,"drinks"));
+
+const drinkList =
+document.getElementById("drinkList");
+
+if(!drinkList) return;
+
+drinkList.innerHTML="";
+
+snapshot.forEach(docSnap=>{
+const data = docSnap.data();
+
+drinkList.innerHTML+=`
+<div class="card">
+
+<h3>${data.name}</h3>
+<p>Price: $${data.price}</p>
+
+</div>
+`;
+});
+
+}

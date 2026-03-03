8cadc18784b6eb91dbb146a1642f7653942e3966
abdifurgasa <abdifurgasa2nd@gmail.com>
2026-03-03 10:52:49 +0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,17 +1,32 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import {
+getAuth
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+import {
+getFirestore
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+import {
+getStorage
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
+
+/* 🔥 Your Firebase Config */
 
 const firebaseConfig = {
- apiKey: "AIzaSyDqB7Ig1rso8OEBIL6ad3OtciFqHIk9TdE",
- authDomain: "wanci-international-loge-43dd3.firebaseapp.com",
- projectId: "wanci-international-loge-43dd3",
- storageBucket: "wanci-international-loge-43dd3.appspot.com",
- messagingSenderId: "441258681939",
- appId: "1:441258681939:web:961697760d09b7234688a6"
+apiKey: "AIzaSyDqB7Ig1rso8OEBIL6ad3OtciFhIk9TdE",
+authDomain: "wanci-international-loge-43dd3.firebaseapp.com",
+projectId: "wanci-international-loge-43dd3",
+storageBucket: "wanci-international-loge-43dd3.appspot.com",
+messagingSenderId: "441258681939",
+appId: "1:441258681939:web:961697760d09b7234688a6"
 };
+
+/* ⭐ Initialize Firebase */
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
